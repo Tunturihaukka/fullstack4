c7b9cfc00d4bebe5c8d6902d1bd8e4a62bc41451
Aku <ap@Aku-MacBook-Air.local>
2023-02-16 18:25:11 +0200

    4.2

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  'env': {
+    'node': true, // process not defined with browser option
+    'commonjs': true,
+    'es2021': true
+  },
+  'extends': 'eslint:recommended',
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ]
+  }
+}
