d1fdd8108e5277026d18f6d1436636422b9103aa
Aku <ap@Aku-MacBook-Air.local>
2023-02-18 14:27:43 +0200

    4.15

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = [
   {
@@ -29,6 +30,13 @@ const blogsInDb = async () => {
   return blogs
 }
 
+const usersInDb = async () => {
+  const users = await User.find({})
+  
+  return users
+}
+
 module.exports = {
-  initialBlogs, nonExistingId, blogsInDb
+  initialBlogs, nonExistingId, blogsInDb,
+  usersInDb
 }
