235aa1d2d1b300302c471c7b296cde7ec10d30fd
Aku <ap@Aku-MacBook-Air.local>
2023-02-20 21:06:46 +0200

    4.18

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
@@ -28,6 +29,7 @@ app.use('/api/users', usersRouter)
   
 app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
